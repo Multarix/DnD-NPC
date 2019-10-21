f93ea213e00783f4ae9c083217d37d90f7b92b4e
Multarix <darien2105@gmail.com>
2019-10-22 00:35:40 +1000

    Updated class information

--- src/classes/npc.js
+++ src/classes/npc.js
@@ -1,19 +1,19 @@
-const rolePicker = require("../functions/rolePicker.js");
-const racePicker = require("../functions/racePicker.js");
-const generateNPC = require("../functions/generate.js");
+const rolePicker = require("../npc/functions/rolePicker.js");
+const racePicker = require("../npc/functions/racePicker.js");
+const generateNPC = require("../npc/functions/generate.js");
 module.exports = class npc {
 	constructor(obj){
 		this.roleType = undefined;
 		this.raceType = undefined;
 		if(obj){
-			if(obj.raceType) this.raceType = racePicker("npc", [obj.raceType, obj.subRace]);
+			if(obj.raceType) this.raceType = racePicker([obj.raceType, obj.subRace]);
 			if(obj.roleType) this.roleType = rolePicker({ roleType: obj.roleType });
 		}
 	}
 
 	raceType(...obj){
 		if(this.character) return;
-		this.raceType = racePicker("npc", obj);
+		this.raceType = racePicker(obj);
 		return this;
 	}
 
