02b4da5417e01b055dc930a8d7785e6bff524703
Multarix <darien2105@gmail.com>
2019-10-22 00:39:51 +1000

    new npc races

---
+++ src/npc/races/aasimar-protector.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+	return {
+		name: "Fallen Aasimar",
+		link: "https://www.dndbeyond.com/races/aasimar#ProtectorAasimar",
+		disposition: ["Good"],
+		speed: 30,
+		size: "Medium",
+		maxAge: 0,
+		minAge: 0,
+		stats: {
+			strength: 0,
+			dexterity: 0,
+			constitution: 0,
+			intelligence: 0,
+			wisdom: 1,
+			charisma: 2,
+		},
+	};
+};
---
+++ src/npc/races/yuanti-pureblood.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+	return {
+		name: "Yuan-Ti Pureblood",
+		link: "https://www.dndbeyond.com/races/",
+		disposition: ["Evil"],
+		speed: 30,
+		size: "Medium",
+		maxAge: 80,
+		minAge: 18,
+		stats: {
+			strength: 0,
+			dexterity: 0,
+			constitution: 0,
+			intelligence: 1,
+			wisdom: 0,
+			charisma: 2,
+		},
+	};
+};
