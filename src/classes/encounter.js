7f2bc8c64a4c74ac9680a5e7db4c322d78527771
Multarix <darien2105@gmail.com>
2019-11-01 23:17:18 +1000

    This still does nothing

--- src/classes/encounter.js
+++ src/classes/encounter.js
@@ -1,7 +1,14 @@
+// const genEncounter = require("../encounter/functions/generate.js");
 module.exports = class encounter {
 	constructor(obj){
 		const num = parseInt(obj);
-		if(!Number.isInteger(num)) throw "Value supplied is not an integer";
-		return;
+		if(!Number.isInteger(num)){
+			const e = new Error();
+			e.name = "TypeError";
+			e.message = "Value supplied is not an integer";
+			throw e;
+		}
+		this.level = num;
+	//	this.generate = () => genEncounter(this.level);
 	}
 };
