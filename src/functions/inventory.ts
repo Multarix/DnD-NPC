e1472d6c69ee6577734178f958de3b42c42241a0
Multarix <darien2105@gmail.com>
2023-05-13 15:02:12 +1000

    Takes a 5th (optional) argument + isDruid

--- src/functions/inventory.ts
+++ src/functions/inventory.ts
@@ -2,7 +2,7 @@ import weaponList from "../objects/weapons.json" assert { type: "json" };
 import armorList from "../objects/armor.json" assert { type: "json" };
 import { RaceData, WeaponTags, ArmorTags, Inventory, WeaponData, ArmorData } from "../../interfaces.js";
 
-export default function inventory(race: RaceData, strength: number, weaponTags: WeaponTags, armorTags: ArmorTags): Inventory {
+export default function inventory(race: RaceData, strength: number, weaponTags: WeaponTags, armorTags: ArmorTags, isDruid: Boolean = false): Inventory {
 	const gear: Inventory = {
 		weapon: weaponList[0],
 		armor: armorList[0],
@@ -16,16 +16,20 @@ export default function inventory(race: RaceData, strength: number, weaponTags: 
 	// Get all of the viable weapons
 	const viableWeapons: WeaponData[] = [];
 	for(const weapon of weaponList){
+		
+		// Specific Weapons
 		if(weaponTags.names.includes(weapon.name)){
 			viableWeapons.push(weapon);
 			continue;
 		}
 		
+		// Simple Weapons
 		if(weaponTags.simple && weapon.simple){
 			viableWeapons.push(weapon);
 			continue;
 		}
 		
+		// Martial Weapons
 		if(weaponTags.martial && !weapon.simple){
 			viableWeapons.push(weapon);
 			continue;
@@ -42,12 +46,12 @@ export default function inventory(race: RaceData, strength: number, weaponTags: 
 	// Get all of the viable armor
 	const viableArmor: ArmorData[] = [];
 	for(const armor of armorList){
+		// Some classes are not proficient with any armor
 		if(armorTags.types.length === 0) break;
-		if(!armor.isMetal && !armorTags.metal){
-			viableArmor.push(armor);
-			continue;
-		}
 		
+		if(isDruid && armor.name.toLowerCase().includes("chain") || armor.name.toLowerCase().includes("plate") ) continue;
+		
+		// If the the person has high enough strength
 		if(armorTags.types.includes(armor.type) && armor.strengthReq < strength){
 			viableArmor.push(armor);
 			continue;
